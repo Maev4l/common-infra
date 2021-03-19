a296582b47bfe6de9b85538d74f5e10f57cbcfc8
Maev4l <maeval.nightingale@gmail.com>
2021-03-19 07:45:33 +0100

    Add cleanup untagged images olders than 7 days

--- images-repositories.tf
+++ images-repositories.tf
@@ -9,3 +9,26 @@ resource "aws_ecr_repository" "image_resize_repository" {
   tags = local.tags
 }
 
+resource "aws_ecr_lifecycle_policy" "image_resize_repository_lifecycle" {
+  repository = aws_ecr_repository.image_resize_repository.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 7 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 7
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
